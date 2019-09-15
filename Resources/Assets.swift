4c77c800f61321f03caf3bfd332592bf84366f87
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-15 13:15:56 -0300

    Reorganizing assets

---
+++ Resources/Assets.swift
@@ -0,0 +1,85 @@
+import UIKit
+
+private class PrivateAssets {}
+
+/// Assets for the project.
+/// Provides an easy, global access to the project resources.
+public struct Assets {
+    
+    /// Colors from the asset catalog
+    public struct Colors {
+        
+        /// TabBar colors
+        public struct TabBar {
+            /// Color to be used for background
+            public static let backgroundColor = color("TabBarBackgroundColor")
+            /// Color to be used for active tab bar item color
+            public static let selectedTextColor = color("TabBarSelectedTextColor")
+            /// Color to be used for default tab bar item color
+            public static let unselectedTextColor = color("TabBarUnselectedTextColor")
+            /// Color to be used for active tab bar item icon
+            public static let selectedIconColor = color("TabBarSelectedIconColor")
+            /// Color to be used for default tab bar item icon
+            public static let unselectedIconColor = color("TabBarUnselectedIconColor")
+        }
+        
+        /// NavigationBar colors
+        public struct NavigationBar {
+            /// Color to be used for background
+            public static let backgroundColor = color("NavigationBarBackgroundColor")
+            /// Color to be used for title
+            public static let titleColor = color("NavigationBarTitleTextColor")
+            /// Color to be used for icons
+            public static let iconColor = color("NavigationBarIconColor")
+        }
+
+    }
+    
+    /// Icons from the asset catalog
+    public struct Icons {
+        
+        private static let selectedIconTintColor = Assets.Colors.TabBar.selectedIconColor ?? UIColor.blue
+        
+        /// Icons for each App Module
+        public struct Modules {
+            /// Icon to be used for favorite module
+            public static let favorite = image("FavoriteMoviesIcon", tint: selectedIconTintColor)
+            /// Icon to be used for catalog module
+            public static let catalog = image("MoviesCatalogIcon", tint: selectedIconTintColor)
+        }
+
+        /// Icon to be used for checked status
+        public static let check = image("CheckmarkIcon")
+        /// Icon to be used for favorite movies
+        public static let favoriteFull = image("FavoriteFullIcon")
+        /// Icon to be used for non-favorite movies
+        public static let favoriteGray = image("FavoriteGrayIcon")
+        
+        /// Icons for each App action
+        public struct Actions {
+            /// Icon for filter action
+            public static let filter = image("FilterIcon")
+            /// Icon for search action
+            public static let search = image("SearchIcon")
+        }
+
+    }
+    
+    private static func color(_ colorName: String) -> UIColor? {
+        return UIColor(named: colorName, in: Bundle(for: PrivateAssets.self), compatibleWith: nil)
+    }
+    
+    private static func color(_ colorName: String, `default`: UIColor) -> UIColor {
+        guard let assetColor = color(colorName) else { return `default` }
+        return assetColor
+    }
+    
+    private static func image(_ imageName: String) -> UIImage? {
+        return UIImage(named: imageName, in: Bundle(for: PrivateAssets.self), compatibleWith: nil)
+    }
+    
+    private static func image(_ imageName: String, tint: UIColor) -> UIImage? {
+        return image(imageName)?.tint(with: tint)
+    }
+
+}
