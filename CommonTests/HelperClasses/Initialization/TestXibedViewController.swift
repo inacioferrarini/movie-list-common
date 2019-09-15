2fa866746aefd09bdac2f4e0a7c6beabd1d8651f
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-15 11:28:20 -0300

    Updates tests

---
+++ CommonTests/HelperClasses/Initialization/TestXibedViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+import Common
+
+class TestXibedViewController: UIViewController {
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    func setup() {
+        title = "TestXibedViewController Title"
+    }
+
+}
+
+extension TestXibedViewController: XibedViewController {}
